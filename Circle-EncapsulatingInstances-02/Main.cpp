0ba8bf15ca076397dfa99dd075f74ca127aa01d4
getMai <MaiRuF@outlook.com>
2019-11-16 16:45:38 +0800

    封装实例-02-圆

--- Circle-EncapsulatingInstances-02/Main.cpp
+++ Circle-EncapsulatingInstances-02/Main.cpp
@@ -2,9 +2,16 @@
 
 int main()
 {
+	// 使用构造函数初始化对象
+	Circle* pC1 = new Circle(0 , 0 , 6);
 
+	pC1->showXYR();
 	
-
+	// 测试点和圆判断关系函数：
+	pC1->pointAndCircle(30, 9);
+	pC1->pointAndCircle(30, 6);
+	pC1->pointAndCircle(30, 1);
+	
 
 	system("pause");
 	return 0;
